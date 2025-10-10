28eecffbdfe64cd93d20b2d574bea4410e09ecde
Martin Mechtel <mechtel@iqb.hu-berlin.de>
2025-10-10 12:54:02 +0200

    Add numeric values to status

--- response.interface.ts
+++ response.interface.ts
@@ -5,6 +5,65 @@ export const responseStatesInOrder = ['UNSET', 'NOT_REACHED', 'DISPLAYED', 'VALU
   'DERIVE_ERROR', 'CODING_COMPLETE', 'PARTLY_DISPLAYED', 'DERIVE_PENDING', 'NO_CODING', 'CODE_SELECTION_PENDING',
   'CODING_INCOMPLETE', 'CODING_ERROR'];
 
+export const responseStatesNumericMap = [
+  {
+    key: 0,
+    value: "UNSET"
+  },
+  {
+    key: 1,
+    value: "NOT_REACHED"
+  },
+  {
+    key: 2,
+    value: "DISPLAYED"
+  },
+  {
+    key: 3,
+    value: "VALUE_CHANGED"
+  },
+  {
+    key: 4,
+    value: "DERIVE_ERROR"
+  },
+  {
+    key: 5,
+    value: "CODING_COMPLETE"
+  },
+  {
+    key: 6,
+    value: "NO_CODING"
+  },
+  {
+    key: 7,
+    value: "INVALID"
+  },
+  {
+    key: 8,
+    value: "CODING_INCOMPLETE"
+  },
+  {
+    key: 9,
+    value: "CODING_ERROR"
+  },
+  {
+    key: 10,
+    value: "PARTLY_DISPLAYED"
+  },
+  {
+    key: 11,
+    value: "DERIVE_PENDING"
+  },
+  {
+    key: 12,
+    value: "INTENDED_INCOMPLETE"
+  },
+  {
+    key: 13,
+    value: "CODE_SELECTION_PENDING"
+  }
+];
+
 export type ResponseValueSingleType = null | string | number | boolean;
 export type ResponseValueType = ResponseValueSingleType | ResponseValueSingleType[];
 
